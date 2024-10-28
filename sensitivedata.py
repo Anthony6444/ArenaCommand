bdb2a8a2335ce6d7926556c2412b7c8278371ca7
Anthony6444 <anthonydc1972@outlook.com>
2024-10-28 18:52:51 -0500

    sensitivedata.py stub

---
+++ sensitivedata.py
@@ -0,0 +1,10 @@
+CHALLONGE_USERNAME = "YOUR USERNAME HERE"
+CHALLONGE_API_KEY = "YOUR API KEY HERE"
+
+
+def get_challonge_username():
+    return CHALLONGE_USERNAME
+
+
+def get_challonge_api_key():
+    return CHALLONGE_API_KEY
